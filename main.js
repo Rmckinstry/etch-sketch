bf99893fab3ad35359a11800e92f66ffdba297a8
Ryan McKinstry <ryanmckinstry2@gmail.com>
2021-08-23 15:48:12 -0500

    added dynamic grid size

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const container = document.querySelector('.container');
 
-for(let i=0;i<16;i++){
+let input = 10;
+let count = input*input;
+
+container.style.gridTemplateColumns = `repeat(${input},1fr)`;
+
+for(let i=0;i<count;i++){
     const div = document.createElement('div')
     container.appendChild(div)
     div.classList.add('div-style')
