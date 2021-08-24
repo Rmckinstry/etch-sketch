1b5c1a069aca768c03bdf205b0f4de8a11499dde
Ryan McKinstry <ryanmckinstry2@gmail.com>
2021-08-24 18:34:40 -0500

    added input validation

--- main.js
+++ main.js
@@ -27,14 +27,22 @@ function clearBoard(){
     do{
         container.removeChild(container.firstChild);
     } while(container.firstChild);
-
-    const newInput = prompt("Enter New Board Size. 1 - 100");
+    
+    
+    let newInput = "";
+    do{
+        newInput = prompt("Enter New Board Size. 1 - 100");
+    }while(!(newInput > 0 && newInput < 101)); //accepts only nums 1-100
+    
     generateBoard(newInput);
 }
 
 // first board code
 const container = document.querySelector('.container');
-let userInput = prompt("Enter Board Size. 1 - 100");
+let userInput = ""
+do{
+    userInput = prompt("Enter Board Size. 1 - 100");
+}while(!(userInput>0 && userInput<101));
 
 generateBoard(userInput)
 
